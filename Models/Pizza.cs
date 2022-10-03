008d0d01e89b3b595d4a0a32d261fc84af51082b
Silvia-Du <silviadu@hotmail.it>
2022-10-03 15:15:49 +0200

    Aggiungere i file di progetto.

---
+++ Models/Pizza.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    [Table("pizza")]
+    public class Pizza
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public float Price { get; set; }
+
+        public Pizza(string name, string description, string image, float price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Image = image;
+            this.Price = price;
+        }
+
+    }
+}
